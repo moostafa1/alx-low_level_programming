52d611b83a99c262a0a73ca6818e12f1df5a549b
Mostafa <desham8@gmail.com>
2023-02-16 19:23:23 -0800

    7th task and readme update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,10 @@ int main(void)
 	long int longInt;
 	long long int long2Int;
 	float floatType;
+
 	printf("Size of a char: %zu byte(s)", sizeof(charType));
 	printf("Size of an int: %zu byte(s)", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)", sizeof(longInt));
 	printf("Size of a long long int: %zu byte(s)", sizeof(long2Int));
 	printf("Size of a float: %zu byte(s)", sizeof(floatType));
-
+	return (0);
