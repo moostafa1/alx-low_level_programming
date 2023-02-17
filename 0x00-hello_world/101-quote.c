bd50e19c89950dbf8de680884d9bb3ef0dd8bfbd
Mostafa <desham8@gmail.com>
2023-02-16 20:55:54 -0800

    101th task and readme update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * Return: Always 1 (Success)
  */
 
-int main(void
+int main(void)
 {
     char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     for(int i=0; i<sizeof(txt); i++)
