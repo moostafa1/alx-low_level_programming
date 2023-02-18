abee15737621a6382e5d7f69adad9131129bbe06
Mostafa <desham8@gmail.com>
2023-02-18 06:21:24 -0800

    5th task and readme update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * alphabet - prints the alphpet except 'q' and 'e' characters
+ * @init: the starting charachter
+ *  @end: the ending character
+ *
+ */
+
+void alphabet(char init, char end)
+{
+	for (; init <= end; init++)
+	{
+		if (strchr("qe", init) == 0)
+			putchar(init);
+	}
+}
+
+
+/**
+ * main - prints text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	alphabet('a', 'z');
+	putchar('\n');
+
+	return (0);
+}
