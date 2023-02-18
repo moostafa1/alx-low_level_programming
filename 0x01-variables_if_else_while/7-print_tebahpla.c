d72d54f41fed6bd7c0480f0f26683bea5240f1e1
Mostafa <desham8@gmail.com>
2023-02-18 08:56:58 -0800

    8th task and readme update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * digits = prints digits from init to end
+ * @init: the starting number
+ * @end : the ending number
+ */
+
+void digits(int init, int end)
+{
+	for (; init < end; init++)
+		putchar(init + '0');
+}
+
 
 
 /**
@@ -14,7 +26,11 @@
  *	alphapet('a', 'd') --> abcd
  */
 
-void alphapet(char init, char end);
+void alphapet(char init, char end)
+{
+	for (; init >= end; init--)
+		putchar(init);
+}
 
 
 
@@ -26,23 +42,9 @@ void alphapet(char init, char end);
 
 int main(void)
 {
-	alphapet('z', 'a');
+	digits(0, 10);
+	alphapet('a', 'f');
 	putchar('\n');
 
 	return (0);
-}
-
-
-
-/**
- * alphapet - prints the alphapet from init to end characters
- * @init : the starting character
- * @end  : the ending character
- *
- */
-
-void alphapet(char init, char end)
-{
-	for (; init >= end; init--)
-		putchar(init);
 }
