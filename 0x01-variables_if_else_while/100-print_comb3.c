6aa0f5c6780624fb08192b4981dc52d04c0631c3
Mostafa <desham8@gmail.com>
2023-02-18 15:53:09 -0800

    100th task and readme update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 {
 	int i = 0;
@@ -23,6 +24,10 @@ int main(void)
 				{
 					putchar(',');
 					putchar(' ');
+				}
+				else
+				{
+					putchar('\n');
 				}
 			}
 			j++;
