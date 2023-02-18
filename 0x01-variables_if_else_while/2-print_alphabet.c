82b0647cbcd44febdab6878291d841fa0d74f336
Mostafa <desham8@gmail.com>
2023-02-17 18:41:50 -0800

    3rd task and readme update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+
+/**
+ * main - prints text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    char a = 'a';
+    
+    for(;a<='z'; a++)
+    {
+	    putchar(a);
+    }
+    
+    return (0);
+}
