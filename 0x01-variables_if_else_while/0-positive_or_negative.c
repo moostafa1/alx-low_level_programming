9b035dbc83d490b9f48fcfb1c433567ea5d7d650
Mostafa <desham8@gmail.com>
2023-02-17 17:29:23 -0800

    1st task and readme update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,14 +12,15 @@ int main(void)
 	
 	if (n>0)
 	{
-		puts("%d is positive", n);
-	else if (n==0))
+		printf("%d is positive", n);
+	}
+	else if (n==0)
 	{
-		puts("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		puts("%d is negative", n);
-	
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
