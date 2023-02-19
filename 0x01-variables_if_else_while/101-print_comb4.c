e8a0f3886bc266968a54cd3c6a60ff10d50432fe
Mostafa <desham8@gmail.com>
2023-02-18 19:22:40 -0800

    101th task and readme update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - prints text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int k = 0;
+
+	while(k<8)
+	{
+		int i = 1;
+		
+		for (; i < 9; i++)
+		{
+			int j = 2;
+		       
+			while (j < 10)
+			{
+				if (k < i && i < j)
+				{
+					putchar(k + '0');
+					putchar(i + '0');
+					putchar(j + '0');
+					if (k != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					else
+					{
+						putchar('\n');
+					}
+				}
+				j++;
+			}
+		}
+		k++;
+	}
+	return (0);
+}
