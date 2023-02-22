a1f45e030d85ab445cda58c3649e35655b3c07e6
Mostafa <desham8@gmail.com>
2023-02-22 13:54:30 -0800

    6th task and readme update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an integer (+ve or -ve)
+ * @n: input integer to get its sign
+ *
+ * Return: 1 if +ve, 0 if zero, -1 if -ve
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0');
+		return (0);
+	}
+	_putchar('-');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(n + '0');
+	return (-1);
+}
