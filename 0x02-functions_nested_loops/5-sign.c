4fd610dec1aea514eda449dabeab21a986064505
Mostafa <desham8@gmail.com>
2023-02-22 14:28:01 -0800

    6th task and readme update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,13 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(1 + '0');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(n + '0');
 		return (0);
 	}
 	_putchar('-');
-	_putchar(1 + '0');
 	return (-1);
 }
