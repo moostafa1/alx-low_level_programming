b435484a2f53b22f0e99f1fa032902fc775c36af
Mostafa <desham8@gmail.com>
2023-02-21 17:00:24 -0800

    1st task and readme update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints text
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+All My c tasks related to ALX Programme are founded here
+0-putchar.c --> prints _putchar, followed by a new line
+
+
