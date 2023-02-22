2a9532663d0056864e88e00637e75847bedbd53d
Mostafa <desham8@gmail.com>
2023-02-22 15:50:32 -0800

    8th task and readme update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+/**
+ * jack_bauer - every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: none
+ */
+
+void jack_bauer(void)
+{
+	int hr1 = 2;
+	int hr2 = 3;
+	int min1 = 5;
+	int min2 = 9;
+	int i = 0;
+
+	for (; i <= hr1; i++)
+	{
+		int j = 0;
+
+		for (; j <= hr2; j++)
+		{
+			int k = 0;
+
+			for (; k <= min1; k++)
+			{
+				int l = 0;
+
+				for (; l <= min2; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
