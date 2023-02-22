0a08f934d4feb844cce03c1b973698520f380123
Mostafa <desham8@gmail.com>
2023-02-21 20:21:34 -0800

    2nd task and readme update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,9 @@
 #include "main.h"
 
 
+
 int _putchar(char c);
-
-/**
- * print_alphabet - prints all lowercase letters
- *
- * Return: none
- */
-void print_alphabet(void)
-{
-	char start = 'a';
-	while (start <= 'z')
-	{
-		_putchar(start);
-		start++;
-	}
-}
-
+void print_alpahbet(void);
 
 
 /**
