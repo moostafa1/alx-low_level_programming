b8a4883ec4dece610865b0a27810927a556cd80d
Mostafa <desham8@gmail.com>
2023-02-22 16:30:43 -0800

    8th task and readme update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,32 @@
 /**
+ * _abs - prints the absolute value of integer
+ * @n: input integer to get its sign
+ *
+ * Return: 1 if +ve, 0 if zero, -1 if -ve
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	return i;
+}
+
+
+
+/**
  * print_last_digit - prints the last digit of an integer)
- * @i: input integer to get its last digit
+ * @n: input integer to get its last digit
  *
  * Return: last digit (0:9)
  */
 
 int print_last_digit(int i)
 {
-	return (i % 10);
+	int x = _abs(i) % 10;
+	_putchar(x + '0');
+	
+	return (x);
 }
