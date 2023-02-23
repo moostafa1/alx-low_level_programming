5a03414fa4bd544e6ff1f7d4bee140ac9de5e630
Mostafa <desham8@gmail.com>
2023-02-22 21:39:11 -0800

    10th task and readme update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+/**
+ * times_table - prints  the 9 times table, starting with 0
+ *
+ * Return: none
+ */
+
+void times_table(void)
+{
+		int i = 0;
+
+		for (; i < 10; i++)
+		{
+			int j = 0;
+
+			for (; j < 10; j++)
+				{
+					int x = i * j;
+
+					if (x < 10)
+					{
+							_putchar(x + '0');
+					}
+					else
+					{
+							_putchar((x / 10) + '0');
+							_putchar((x % 10) + '0');
+					}
+
+					if (j < 9)
+					{
+						_putchar(',');
+					}
+					if (((j + 1) * i) < 10)
+					{
+						_putchar(' ');
+					}
+					_putchar(' ');
+				}
+				_putchar('\n');
+		}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,2 @@
 int _putchar(char c);
-int add(int, int);
+void times_table(void);
