de6586c30a856708edd8de2a382916323a7bc8ed
Mostafa <desham8@gmail.com>
2023-02-24 15:23:26 -0800

    5th task and readme update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, except 2 and 4
+ * print_most_numbers - prints the numbers, from 0 to 9, except 2 and 4
  *
  * Return: none
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 48;
 
@@ -16,3 +16,4 @@ void print_numbers(void)
 	}
 	_putchar('\n');
 }
+
