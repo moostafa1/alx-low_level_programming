d336a5a7e33ed8c7a827022f16baa0c7f4dd1eae
Mostafa <desham8@gmail.com>
2023-02-24 15:50:58 -0800

    6th task and readme update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,7 @@ _putit(int i)
 
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  *
  * Return: none
  */
