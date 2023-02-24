bb8a407ae69862ff1559b2fede77228ca21ddba4
Mostafa <desham8@gmail.com>
2023-02-24 15:49:23 -0800

    6th task and readme update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _putit - prints the character given
+ * @i: character to print
+ *
+ * Return: none
+ */
+_putit(int i)
+{
+	_putchar(i);
+}
+
+
+
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line
+ *
+ * Return: none
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		int j = 0;
+
+		for (; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putit(j / 10 + '0');
+				_putit(j % 10 + '0');
+			}
+			else
+			{
+				_putit(j + '0');
+			}
+			if (j == 14 && i != 9)
+			{
+				_putit('\n');
+			}
+		}
+
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -13,3 +13,6 @@
 4-print_most_numbers.c --> prints the numbers, from 0 to 9, except 2 and 4, followed by a new line
 
 
+5-more_numbers.c --> function that prints 10 times the numbers, from 0 to 14, followed by a new line
+
+
