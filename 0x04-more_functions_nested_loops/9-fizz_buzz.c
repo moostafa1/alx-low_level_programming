7e90dab7001e1cb118d30b170fd827df4953d271
Mostafa <desham8@gmail.com>
2023-02-24 18:26:07 -0800

    10th task and readme update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz_test - prints the numbers from 1 to n, followed by a new line.
+ * for multiples of three print Fizz instead of the number
+ * for the multiples of five print Buzz
+ * for numbers which are multiples of both three and five print FizzBuzz
+ * @s: the starting digit
+ * @n: the maximum limit
+ *
+ * Return: none
+ */
+void fizz_buzz_test(int s, int n)
+{
+
+	for (; s <= n; s++)
+	{
+		if (s % 3 == 0 && s % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (s % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (s % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", s);
+
+		}
+		if (s != n)
+		{
+			putchar(' ');
+		}
+	}
+}
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	fizz_buzz_test(1, 100);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -22,3 +22,6 @@
 7-print_diagonal.c --> function that draws a diagonal line on the terminal
 
 
+9-fizz_buzz.c --> The “Fizz-Buzz test” is an interview question designed to help filter out the 99.5% of programming job candidates who can’t seem to program their way out of a wet paper bag --- Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz {Each number or word should be separated by a space}
+
+
