6a918c9996b41398aec785710d2ccdbd5db49bd0
Mostafa <desham8@gmail.com>
2023-02-27 18:12:41 -0800

    6th task and readme update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string -   prints a string, in reverse, followed by a new line
+ * @s: the character array (string)
+ *
+ * Return: none
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (; i >= 0; i--)
+	{
+
+		if (s[i] == '\0')
+		{
+			continue;
+		}
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
