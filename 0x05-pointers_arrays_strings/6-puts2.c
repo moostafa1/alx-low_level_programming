f5acb3c563c267f191708fa10223d92b44ec788d
Mostafa <desham8@gmail.com>
2023-02-27 18:41:07 -0800

    7th task and readme update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * character
+ * @str: the character array (string)
+ *
+ * Return: none
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+}
