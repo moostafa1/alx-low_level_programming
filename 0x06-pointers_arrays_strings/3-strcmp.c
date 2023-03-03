03f366734e5fa09d4fef946d322f7d9d85d7778d
Mostafa <desham8@gmail.com>
2023-03-02 17:42:43 -0800

    4th task and readme update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,24 +10,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int len_s1 = 0;
-	int len_s2 = 0;
 
 	while (s1[len_s1] != '\0')
 	{
+		if (s1[len_s1] == s2[len_s1])
+		{}
+		else
+		{
+			return (s1[len_s1] - s2[len_s1]);
+		}
 		len_s1++;
 	}
 
-	while (s2[len_s2] != '\0')
-	{
-		len_s2++;
-	}
-	if (len_s1 < len_s2)
-	{
-		return (-15);
-	}
-	else if (len_s1 > len_s2)
-	{
-		return (15);
-	}
 	return (0);
 }
