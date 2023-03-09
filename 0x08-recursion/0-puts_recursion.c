ed04e45ac9f9cca50fad266f217f2028625b0d55
Mostafa <desham8@gmail.com>
2023-03-09 12:31:47 -0800

    1st task and readme update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _puts_recursion - prints text followed by new line
+ * @s: text to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
