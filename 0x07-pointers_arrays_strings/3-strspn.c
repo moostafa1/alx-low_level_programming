0cfd1ba3d08c19f42254c3fdd2de75e40067fba1
Mostafa <desham8@gmail.com>
2023-03-12 14:41:41 -0700

    4th task and readme update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: input string
- * @accept: 2nd string
+ * @accept: 2nd string to compare
  *
  * Return: number of bytes in the initial segment of s
  * which consist only of bytes from accept
@@ -11,18 +11,24 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
-	char arr[8] = {'/', ' ', '\t', ',', '\n', '\\', ';', ':'};
+	unsigned int cnt = 0;
 
 	for (; *(s + i) != '\0'; i++)
 	{
 		unsigned int j = 0;
 
-		for (; j < 8; j++)
+		if (*(s + i) == ' ' || *(s + i) == ',')
 		{
-			if (*(s + i) == *(arr + j))
+			break;
+		}
+
+		for (; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
 			{
-				return (i);
+				cnt++;
 			}
 		}
 	}
+	return (cnt);
 }
