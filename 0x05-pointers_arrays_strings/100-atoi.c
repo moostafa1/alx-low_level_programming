a9cd810fb5db2b4a1b6457c2dddae3921b09efde
Mostafa <desham8@gmail.com>
2023-03-12 22:04:29 -0700

    1st advanced task and readme update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -34,6 +34,8 @@ int _atoi(char *s)
 	int i = 0;
 	int len = 0;
 	int flag = 0;
+	int j = 0;
+	int str_to_int = 0;
 
 	for (; s[len] != '\0';)
 		len++;
@@ -50,15 +52,12 @@ int _atoi(char *s)
 		}
 	}
 
-	int j = 0;
-	int str_to_int = 0;
 
 	for (; j < i; j++)
 	{
 		int cont = *(s + j) - '0';
 
 		str_to_int += (cont * _pow_recursion(10, i - 1 - j));
-		int x = _pow_recursion(10, i - 1 - j);
 	}
 
 	if (flag == 0)
