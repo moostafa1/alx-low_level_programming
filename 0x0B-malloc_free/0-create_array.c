754addc1fd1972df036f197e9b3649dde94a70d6
Mostafa <desham8@gmail.com>
2023-03-15 06:35:04 -0700

    1st task and readme update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
+
+
+/**
+ * create_attay - creates array with size n and values c
+ * @size: size of array
+ * @c: charactet to initialize array with
+ *
+ * Return: pointer to the array, else NULL
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *s;
 
 	s = malloc(sizeof(*s) * size);
-	if (size == 0 || s == '\0')
+	if (size == 0 || s == NULL)
 	{
-		return ('\0');
+		return (NULL);
 	}
-	s = c;
+	*s = c;
 	free(s);
 	return (s);
 }
-
