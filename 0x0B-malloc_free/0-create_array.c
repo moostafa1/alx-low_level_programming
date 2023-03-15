7d2c9de3b5f53aca7082f87dd612dc8a752a9624
Mostafa <desham8@gmail.com>
2023-03-15 06:20:26 -0700

    1st task and readme update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,11 +5,12 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	s = malloc(sizeof(*s) * size);
-	if (size == 0)
+	if (size == 0 || s == '\0')
 	{
 		return ('\0');
 	}
-	*s = c;
+	s = c;
+	free(s);
 	return (s);
 }
 
