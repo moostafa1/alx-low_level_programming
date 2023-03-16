22b8cf98a10759722bf427adde5d99a6da537ec3
Mostafa <desham8@gmail.com>
2023-03-16 14:41:28 -0700

    4th task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,40 +2,49 @@
 
 /**
  * alloc_grid - returns pointer to a 2d array
- * @width: number of columns
- * @height: number of rows
+ * @width: array width (number of columns)
+ * @height: array height (number of rows)
  *
  *Return: pointer to a 2 dimensional array of integers
  */
 int **alloc_grid(int width, int height)
 {
-	int *arr_2d;
-	int **arr_p;
+	int **arr_2d;
 	int r = 0;
 
-	arr_p = &arr_2d;
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr_2d = malloc(sizeof(*arr_2d) * (width + 1) * (height + 1));
+	arr_2d = malloc(sizeof(*arr_2d) * height);
+
 	if (arr_2d == NULL)
 	{
+		free(arr_2d);
 		return (NULL);
 	}
 
 	for (; r < height; r++)
 	{
+		*(arr_2d + r) = malloc(sizeof(*arr_2d) * width);
+
+		if (*(arr_2d + r) == NULL)
+		{
+			for (; r >= 0; r--)
+				free(*(arr_2d + r));
+
+			free(arr_2d);
+			return (NULL);
+		}
+	}
+
+	for (r = 0; r < height; r++)
+	{
 		int c = 0;
 
 		for (; c < width; c++)
 		{
-			*(arr_2d + r * height + c) = 0;
+		*((*arr_2d + r) + r * width + c) = 0;
 		}
 	}
-	return (arr_p);
+	return (arr_2d);
 }
-
-
-
-
-
