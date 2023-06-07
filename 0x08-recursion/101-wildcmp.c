65225d3dba63b2bacfa66cf959b10ec406aeca5f
mostafa <desham8@gmail.com>
2023-06-08 01:11:50 +0300

    fixed 101-wildcard code

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,41 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
-
-
-
-
-/**
- * wildcmp - Compare two strings allowing for wildcard char
- * @s1: String being compared
- * @s2: String being compared against
- *
- * Return: 1 if considered identical, 0 otherwise
- */
- /*
-int wildcmp(char *s1, char *s2)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1));
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-			return (1);
-	}
-	return (0);
-}
-*/
-
+#include "main.h"
 
 
 
 
 
 /**
- * incremental - compares two strings using wildcard
+ * wildcmp - compares two strings using wildcard
  * @s1: 1st string
  * @s2: 2nd string with wildcard '*'
  *
@@ -60,64 +30,3 @@ int wildcmp(char *s1, char *s2)
 
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
-
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "ma********************************c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "***");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m.*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "**.*c");
-    printf("%d\n", r);
-    r = wildcmp("main-main.c", "ma*in.c");
-    printf("%d\n", r);
-    r = wildcmp("main", "main*d");
-    printf("%d\n", r);
-    r = wildcmp("abc", "*b");
-    printf("%d\n", r);
-    return (0);
-}
-
-
-
-/*
-1
-1
-1
-1
-1
-1
-1
-0
-1
-1
-0
-0
-*/
