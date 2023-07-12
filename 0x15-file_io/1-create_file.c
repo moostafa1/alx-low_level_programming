03017e2873005f5a345002eac724568c99f7b28b
mostafa <desham8@gmail.com>
2023-07-12 16:19:02 -0700

    creating a file fixing errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fw = write(fo, text_content, sizeof(text_content));
 
 	if (fo == -1 || fw == -1)
