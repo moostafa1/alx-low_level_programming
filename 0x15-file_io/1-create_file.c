c7ade813002447b63f32f239adb5165d5b4077db
mostafa <desham8@gmail.com>
2023-07-12 10:56:07 -0700

    create new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file = 0, r = 0, w = 0;
+	FILE *fptr;
+	int flag = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY);
+	fptr = fopen(filename, "a+");
 
+	if (!fptr)
+		return (-1);
 
+	flag = fwrite(text_content, sizeof(text_content), sizeof(text_content), fptr);
+	if (!flag)
+		return (-1);
+	fclose(fptr);
 
+	return (1);
 }
