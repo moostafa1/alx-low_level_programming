a07db7c6d33cab69df7eebc04dd82fd022a5fde6
mostafa <desham8@gmail.com>
2023-07-12 11:23:53 -0700

    fixing logic

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fptr = fopen(filename, "w+");
+	fptr = fopen(filename, "a+");
 
 	if (!fptr)
 		return (-1);
-	if (!text_content)
-		text_content = '\0';
 
 	flag = fwrite(text_content, sizeof(text_content), sizeof(text_content), fptr);
 	if (!flag)
