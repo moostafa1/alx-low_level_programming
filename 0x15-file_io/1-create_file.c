6f1c228b510ba7f70eea70f71e88c6e3a8d6a21a
mostafa <desham8@gmail.com>
2023-07-12 16:23:17 -0700

    fixing logical error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		len = sizeof(text_content);
+		len = sizeof(text_content) - 1;
 	}
 
 	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
