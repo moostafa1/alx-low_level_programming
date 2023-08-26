df47649b4558466681a6e1c03ab8726fd27d826a
mostafa <desham8@gmail.com>
2023-08-26 16:25:06 -0700

    fixing logical error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	p = *head;
-	for (; i < idx - 1 && p; i++)
+	for (; i < idx - 1 && p != NULL; i++)
 		p = p->next;
 	if (!p)
 		return (NULL);
