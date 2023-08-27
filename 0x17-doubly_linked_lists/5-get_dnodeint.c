557c6b066adb1a321069269c2f6618758ddb3a98
root <root@DESKTOP-K1NJQMC.localdomain>
2023-08-28 01:08:04 +0200

    get node at specific index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns node with specific index
+ * @head: pointer to the first element of doubly linked list
+ * index: position of the element to remove
+ *
+ * Return: nth node of a dlistint_t linked list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+
+	for (; head; i++)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
+}
