0036d496631dddc61692166aa3534ae2de35a7c7
root <root@DESKTOP-K1NJQMC.localdomain>
2023-08-28 03:34:26 +0200

    betty code style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp->next = p->next;
 		p->next = temp;
 		temp->prev = p;
-		return(temp);
+		return (temp);
 	}
 	if (!*h)
 	{
