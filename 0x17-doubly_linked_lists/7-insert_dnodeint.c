18302005e70e2f5c7a22fb37926b2380cd1f2538
Mostafa <desham8@gmail.com>
2024-01-16 01:52:38 +0200

    meeting betty code style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,48 @@
 #include "lists.h"
 
+
+/**
+ * insert_node_begin_end - adds a node either at beginning or at end
+ * to a doubly linked list
+ * @h: position of the 1st node of a doubly linked list
+ * @temp: temporary pointer to the head
+ * @idx: position to add element in it
+ * @len: length of the list
+ * @n: value to add
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_node_begin_end(dlistint_t **h, dlistint_t *temp,
+		unsigned int idx, unsigned int len, int n)
+{
+	if (idx > len)
+	{
+		free(temp);
+		return (NULL);
+	}
+
+	temp->n = n;
+	temp->next = NULL;
+	temp->prev = NULL;
+
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+		return (temp);
+	}
+
+	if (idx == len)
+	{
+		add_dnodeint_end(h, n);
+		return (temp);
+	}
+
+	return (temp);
+}
+
+
+
+
 /**
  * insert_dnodeint_at_index - adds a node at a specific position
  * @h: position of the 1st node of a doubly linked list
@@ -16,30 +59,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp = malloc(sizeof(temp));
 	if (!h || !temp)
 		return (NULL);
+
 	p = *h;
 	for (; p; len++)
 		p = p->next;
-	if (idx > len)
-	{
-		free(temp);
-		return (NULL);
-	}
-	temp->n = n;
-	temp->next = NULL;
-	temp->prev = NULL;
-	if (idx == 0)
-	{
-		add_dnodeint(h, n);
-		return (temp);
-	}
-	if (idx == len)
-	{
-		add_dnodeint_end(h, n);
-		return (temp);
-	}
+
+	insert_node_begin_end(h, temp, idx, len, n);
+
 	p = *h;
 	for (len = 0; p && len < idx - 1; len++)
 		p = p->next;
+
 	if (p)
 	{
 		temp->next = p->next;
@@ -52,5 +82,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(temp);
 		return (NULL);
 	}
+
 	return (temp);
 }
